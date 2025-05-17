bc86425252b1e226a5a3e0243b548f0849653e67
Emran BinJamaan <therealemran.design@gmail.com>
2025-05-17 10:59:05 +0200

    refac: minimal program structure

--- src/main.c
+++ src/main.c
@@ -1,34 +1,19 @@
 #include "psniff.h"
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char *argv[]) {
 	if (argc == 4) {
-		const char* source = argv[1];
-		const char* mode = argv[2];
-		const char* output = argv[3]; (void)output; //// TODO
+		// TODO pcap init logic
+		// TODO hashmap create logic
+		static t_context cxt;// = (t_context){0}; // whether this or memset is faster remains to be seen.
 
-		pcap_t* handle;
-		char errbuf[PCAP_ERRBUF_SIZE];
-	
-		if (strcmp(mode, "live") == 0) {
-			handle = pcap_create(source, errbuf);
-		} else if (strcmp(mode, "file") == 0) {
-			handle = pcap_open_offline(source, errbuf);
-		} else {
-			fprintf(stderr, "Unknown mode: %s\n", mode);
-			return 1;
-		}
+		ps_queue_init(&cxt.queue);
 
-		if (handle == NULL) {
-			fprintf(stderr, "Error opening %s: %s\n", source, errbuf);
-			return 1;
-		}
+		ps_threads_init(&cxt);
+		ps_threads_join(&cxt);
 
-		// TODO: Read packets from file and write to output
-		printf("[+] Reading packets from %s\n", source);
-
-		pcap_close(handle);
+		// Cleanup
+		ps_queue_destroy(&cxt.queue);
 		return 0;
 	}
 	fprintf(stderr, "Usage: %s <interface|pcapfile> <\"live\"|\"file\"> <output_file>\n", argv[0]);
