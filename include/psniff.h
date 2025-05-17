08cbd6c6ce1948851a0fe016e1a8a89a15bd988a
Emran BinJamaan <therealemran.design@gmail.com>
2025-05-17 11:44:59 +0200

    add: tcp connection tracking helper-struct. To be used with the connection map.

--- include/psniff.h
+++ include/psniff.h
@@ -9,15 +9,37 @@
 
 /* ----- Defines ------ */
 
+/* ----- Typedefs & Structs ------ */
+
+typedef struct s_tcp_conn {
+    struct in_addr src_ip;
+    struct in_addr dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+    
+    struct timeval start_time;
+    struct timeval end_time;
+    
+    uint32_t packets_in;
+    uint32_t packets_out;
+    
+    int is_active;
+    uint8_t flags;        // Keep track of TCP flags seen: might be useful later...
+} t_tcp_conn;
+
 typedef struct s_context {
+    pcap_t *handle;
+    FILE *output_file;
+    char *dev_name;
+    
     t_packet_queue queue;
-    pcap_t* pcap;
-    struct {
-        volatile bool running;
-        pthread_t producer;
-        pthread_t consumer;
-    } threads;
-    // t_hashmap* map; // later
+    
+    pthread_t producer_tid;
+    pthread_t consumer_tid;
+    
+    // t_tcp_conn *connections;  // TODO
+    
+    volatile bool running;
 } t_context;
 
 #endif // !PSNIFF_H
