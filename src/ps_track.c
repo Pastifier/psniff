7838c5934e00aa16bdfeaf12d9e26bf70db52969
Emran BinJamaan <therealemran.design@gmail.com>
2025-05-19 09:24:19 +0200

    fix: faulty condition in for-loop in find_or_create_connection() causing the connection-tracking loop to never execute.

--- src/ps_track.c
+++ src/ps_track.c
@@ -98,7 +98,7 @@ int find_or_create_connection(t_context *ctx, t_parsed_packet *parsed) {
     
     // pthread_mutex_lock(&ctx->conn_mutex);
     
-    for (uint32_t i = hash_idx; i < hash_idx /* + _PS_MAX_CONN/10 */; i++) { // DECISION: premature optimisation. Play with the percentage probed as you test.
+    for (uint32_t i = hash_idx; i < _PS_MAX_CONN /* hash_idx  + _PS_MAX_CONN/10 */; i++) {  // DECISION: premature optimisation. Play with the percentage probed as you test.
         int idx = i /* % _PS_MAX_CONN */ ;
         
         int match = is_same_connection(&ctx->connections[idx], 
